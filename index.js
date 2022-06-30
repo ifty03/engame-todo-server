1870737cc0f8dcffa6e81909f209218040d7fb7c
ifty03 <ashikulislamb84@gmail.com>
2022-06-30 22:57:19 +0600

    added task

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -11,6 +12,32 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const uri =
+  "mongodb+srv://dbuser2:dsyvOakmQtRDZB5I@cluster0.ldvxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  await client.connect();
+  try {
+    const taskCollection = client.db("taskDB").collection("task");
+
+    // add task in database
+    app.post("/addTask", async (req, res) => {
+      const task = req.body;
+      console.log(task);
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+  } finally {
+    // await client.close(console.dir)
+  }
+};
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
